6719c7202bd3dfdec3ed33609a75ba04a21ecade
DevJun <guswns1659@gmail.com>
2020-11-03 14:49:54 +0900

    feat : AppConfig 리펙터링

--- src/main/java/com/titanic/springstudy/AppConfig.java
+++ src/main/java/com/titanic/springstudy/AppConfig.java
@@ -1,6 +1,8 @@
 package com.titanic.springstudy;
 
+import com.titanic.springstudy.discount.DiscountPolicy;
 import com.titanic.springstudy.discount.FixDiscountPolicy;
+import com.titanic.springstudy.member.MemberRepository;
 import com.titanic.springstudy.member.MemberService;
 import com.titanic.springstudy.member.MemberServiceImpl;
 import com.titanic.springstudy.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.titanic.springstudy.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
